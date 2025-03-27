23ff4c815755aeab4fef01f873fe4bb2935a2977
Stefan Kewatt <stefan.kewatt@gmail.com>
2025-03-27 10:00:37 -0500

    update init_fns.rs

--- examples/init_fns.rs
+++ examples/init_fns.rs
@@ -1,6 +1,6 @@
 fn main() -> i32 {
 
-	initscr();			/* Start curses mode 		*/
+	let mut scr = init_screen();/* Start curses mode 	*/
 	raw();				/* Line buffering disabled	*/
 	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
 	noecho();			/* Don't echo() while we do getch */
