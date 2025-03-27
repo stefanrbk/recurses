214faeee0e9d6ebb219a49ecbcdfbccd650f9596
Stefan Kewatt <stefan.kewatt@gmail.com>
2025-03-27 10:28:36 -0500

    update simple_print.rs

--- examples/simple_print.rs
+++ examples/simple_print.rs
@@ -3,7 +3,7 @@ fn main() -> i32
     let msg = "Just a string";/* message to be appeared on the screen */
     let scr = init_screen();				/* start the curses mode */
     let (row, col) = scr.getmaxyx();/* get the number of rows and columns */
-    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+    print!(row/2,(col-msg.len())/2,"%s",msg);
                                 	/* print the message at the center of the screen */
  mvprintw(row-2,0,"This screen has %d rows and %d columns\n",row,col);
  printw("Try resizing your window(if possible) and then run this program again");
