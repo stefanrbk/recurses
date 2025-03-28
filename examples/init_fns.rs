1e0f989e4b10af1e75e274621844912040b5b1f7
Stefan Kewatt <stefan.kewatt@gmail.com>
2025-03-28 09:50:29 -0500

    update init_fns.rs

--- examples/init_fns.rs
+++ examples/init_fns.rs
@@ -10,18 +10,18 @@ fn main() -> i32 {
 					 * we have to press enter before it
 					 * gets to the program 		*/
 	if(ch == key_fn!(1))		/* Without keypad enabled this will */
-		print!(s"F1 Key pressed");/*  not get to us either	*/
+		print!(scr, "F1 Key pressed");/*  not get to us either	*/
 					/* Without noecho() some ugly escape
 					 * charachters might have been printed
 					 * on screen			*/
 	else
 	{	print!(scr, "The pressed key is ");
-		scr.set_attr(Bold);
+		scr.attr_on(Bold);
 		print!(scr, "%c", ch);
-		scr.clr_attr(Bold);
+		scr.attr_off(Bold);
 	}
 	scr.refresh();			/* Print it on to the real screen */
-    	getch();			/* Wait for user input */
+    	scr.getch();			/* Wait for user input */
 	scr.close();			/* End curses mode		  */
 
 	return 0;
