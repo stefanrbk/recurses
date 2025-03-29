7cd7ce523ffb48e0307d3f2daf6230b20efd2fe3
Stefan Kewatt <stefan.kewatt@gmail.com>
2025-03-29 10:01:00 -0500

    update simple_scan.rs

--- examples/simple_scan.rs
+++ examples/simple_scan.rs
@@ -3,9 +3,8 @@ fn main() -> i32 {
  
     let mut scr = init_screeb();/* start the curses mode */
     let (row, col) = scr.getmaxyx();/* get the number of rows and columns */
-    let mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
-                     		/* print the message at the center of the screen */
- getstr(str);
+    let str_result = scr.mvprint(row/2,(col-strlen(mesg))/2, format!("%s",mesg))/* print the message at the center of the screen */
+        .getstr(str);
  mvprintw(LINES - 2, 0, "You Entered: %s", str);
  getch();
  endwin();
