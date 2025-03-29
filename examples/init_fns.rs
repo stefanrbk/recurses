bd2403464480bb38d5d93b63199a922c10598fbd
Stefan Kewatt <stefan.kewatt@gmail.com>
2025-03-29 09:46:06 -0500

    update init_fns.rs

--- examples/init_fns.rs
+++ examples/init_fns.rs
@@ -10,13 +10,13 @@ fn main() -> i32 {
 					 * we have to press enter before it
 					 * gets to the program 		*/
 	if(ch == key_fn!(1))		/* Without keypad enabled this will */
-		print!(scr, "F1 Key pressed");/*  not get to us either	*/
+		scr.print("F1 Key pressed");/*  not get to us either	*/
 					/* Without noecho() some ugly escape
 					 * charachters might have been printed
 					 * on screen			*/
 	else
-	{	print!(scr, "The pressed key is ");
-		scr.attr_on(Bold);
+	{	scr.print("The pressed key is ")
+		    .attr_on(Bold);
 		print!(scr, "%c", ch);
 		scr.attr_off(Bold);
 	}
