7e50c0c65b81992137f852a4f33df3e8f2f07e05
Stefan Kewatt <stefan.kewatt@gmail.com>
2025-03-31 09:00:33 -0500

    update simple_attr.rs

--- examples/simple_attr.rs
+++ examples/simple_attr.rs
@@ -1,45 +1,61 @@
-fn main() -> i32
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main()
 {
     let args: Vec<String> = env::args().collect();
     if(args.len() != 2) {
-        print("Usage: %s <a c file name>\n", argv[0]);
-    exit(1);
-  }
-  fp = fopen(argv[1], "r");
-  if(fp == NULL)
-  {
-    perror("Cannot open input file");
-    exit(1);
-  }
-  initscr();				/* Start curses mode */
-  getmaxyx(stdscr, row, col);		/* find the boundaries of the screeen */
-  while((ch = fgetc(fp)) != EOF)	/* read the file till we reach the end */
-  {
-    getyx(stdscr, y, x);		/* get the current curser position */
-    if(y == (row - 1))			/* are we are at the end of the screen */
-    {
-      printw("<-Press Any Key->");	/* tell the user to press a key */
-      getch();
-      clear();				/* clear the screen */
-      move(0, 0);			/* start at the beginning of the screen */
+        print!("Usage: {args[0]} <file name>\n");
+        std::process::exit(1);
     }
-    if(prev == '/' && ch == '*')    	/* If it is / and * then only
+    
+    // Create a path to the desired file
+    let path = Path::new(args[1]);
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => {
+            print!("couldn't open {display}: {why}");
+            std::process::exit(1)},
+        Ok(file) => file,
+    };
+    
+    let mut f = BufReader::new(file);
+    
+    let mut scr = init_screen();				/* Start curses mode */
+    let (row, col) = scr.getmaxyx();		/* find the boundaries of the screeen */
+
+    let mut prev = '\0';
+    for line in f.lines() {
+    	/* read the file till we reach the end */
+        for ch in line.expect("lines failed").chars() {
+            let (y, x) = scr.getyx();		/* get the current curser position */
+            if(y == (row - 1))			/* are we are at the end of the screen */
+            {
+                scr.print("<-Press Any Key->")	/* tell the user to press a key */
+                    .getanych()
+                    .clear()				/* clear the screen */
+                    .move(0, 0);			/* start at the beginning of the screen */
+            }
+            if(prev == '/' && ch == '*')    	/* If it is / and * then only
                                      	 * switch bold on */    
-    {
-      attron(A_BOLD);			/* cut bold on */
-      getyx(stdscr, y, x);		/* get the current curser position */
-      move(y, x - 1);			/* back up one space */
-      printw("%c%c", '/', ch); 		/* The actual printing is done here */
-    }
-    else
-      printw("%c", ch);
-    refresh();
-    if(prev == '*' && ch == '/')
-      attroff(A_BOLD);        		/* Switch it off once we got *
+            {
+                scr.attr_on(Attr::BOLD)			/* cut bold on */
+                
+                    .move(y, x - 1)			/* back up one space */
+                    .print("/*"); 		/* The actual printing is done here */
+            }
+            else
+                scr.print("{ch}");
+            scr.refresh();
+            
+            if(prev == '*' && ch == '/')
+                scr.attr_off(Attr::BOLD);        		/* Switch it off once we got *
                                  	 * and then / */
-    prev = ch;
-  }
-  endwin();                       	/* End curses mode */
-  fclose(fp);
-  return 0;
+            prev = ch;
+        }
+    }
+    scr.close();                       	/* End curses mode */
 }
