cd86be4b24fdffca3e487278c0782cef692e9748
Stefan Kewatt <stefan.kewatt@gmail.com>
2025-03-31 11:31:10 -0500

    update window_border.rs

--- examples/window_border.rs
+++ examples/window_border.rs
@@ -12,9 +12,9 @@ fn main() {
 	let width = 10;
 	let starty = (LINES - height) / 2;	/* Calculating for a center placement */
 	let startx = (COLS - width) / 2;	/* of the window		*/
-	scr.print("Press F1 to exit");
-	refresh();
-	my_win = create_newwin(height, width, starty, startx);
+	scr.print("Press F1 to exit")
+	    .refresh();
+	let mut my_win = Window::new(height, width, starty, startx);
 
 	while((ch = getch()) != KEY_F(1))
 	{	switch(ch)
