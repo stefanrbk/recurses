a64a6d8ed99e3fa0a5b57fc9f9fc30ba4963c3e7
Stefan Kewatt <stefan.kewatt@gmail.com>
2025-04-10 08:01:19 -0500

    update init_fns.rs

--- examples/init_fns.rs
+++ examples/init_fns.rs
@@ -1,9 +1,9 @@
 fn main() {
 
 	let mut scr = Terminal::init_screen();/* Start curses mode */
-	raw();				/* Line buffering disabled	*/
-	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
-	noecho();			/* Don't echo() while we do getch */
+	scr.raw()				/* Line buffering disabled	*/
+	    .keypad(true)		/* We get F1, F2 etc..		*/
+	    .noecho();			/* Don't echo() while we do getch */
 
     let ch = scr.println("Type any character to see it in bold")
 	    .getch();			/* If raw() hadn't been called
